ce80ad60135dbe57b7d6338925333261c8c10df9
Ferdi Durgun <ferdidurgun34@gmail.com>
2024-07-21 18:54:32 +0300

    Update todolist.dart
    If the list is empty, a message will be written in the middle of the screen. Only if else added
    
    Not: Sadece en başa şart koyup ağacı formatladım. Bir sürü kodu kaldrımış gibi görünüm kazandı.

--- lib/todolist.dart
+++ lib/todolist.dart
@@ -53,117 +53,128 @@ class _ToDoListState extends State<ToDoList> {
           ),
         ),
       ),
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          alignment: Alignment.topLeft,
-          children: [
-            ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: notes.length,
-              itemBuilder: (context, index) {
-                final note = notes[index];
-                return Container(
-                  margin: const EdgeInsets.symmetric(vertical: 4),
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: const Color(0xffffffff),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.circular(4.0),
-                    border: Border.all(color: const Color(0x859d9b9b), width: 1),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text(
-                        note.title,
-                        textAlign: TextAlign.start,
-                        maxLines: 2,
-                        overflow: TextOverflow.clip,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
-                          color: Color(0xff000000),
+      body: notes.isEmpty
+          ? const Padding(
+              padding: EdgeInsets.all(16),
+              child: Center(
+                child: Text(
+                  'Yeni bir kayıt eklemek için + butonuna tıklayın.',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ))
+          : SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                alignment: Alignment.topLeft,
+                children: [
+                  ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: notes.length,
+                    itemBuilder: (context, index) {
+                      final note = notes[index];
+                      return Container(
+                        margin: const EdgeInsets.symmetric(vertical: 4),
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: const Color(0xffffffff),
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(4.0),
+                          border: Border.all(
+                              color: const Color(0x859d9b9b), width: 1),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-                        child: Text(
-                          note.content,
-                          textAlign: TextAlign.start,
-                          maxLines: 2,
-                          overflow: TextOverflow.clip,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 14,
-                            color: Color(0xff000000),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
                           children: [
                             Text(
-                              note.time,
+                              note.title,
                               textAlign: TextAlign.start,
-                              maxLines: 1,
+                              maxLines: 2,
                               overflow: TextOverflow.clip,
                               style: const TextStyle(
-                                fontWeight: FontWeight.w400,
+                                fontWeight: FontWeight.w700,
                                 fontStyle: FontStyle.normal,
-                                fontSize: 12,
-                                color: Color(0xff696868),
+                                fontSize: 14,
+                                color: Color(0xff000000),
                               ),
                             ),
-                            Row(
-                              children: [
-                                IconButton(
-                                  icon: const Icon(
-                                    Icons.edit,
-                                    color: Color(0xff212435),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                              child: Text(
+                                note.content,
+                                textAlign: TextAlign.start,
+                                maxLines: 2,
+                                overflow: TextOverflow.clip,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 14,
+                                  color: Color(0xff000000),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    note.time,
+                                    textAlign: TextAlign.start,
+                                    maxLines: 1,
+                                    overflow: TextOverflow.clip,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                      fontStyle: FontStyle.normal,
+                                      fontSize: 12,
+                                      color: Color(0xff696868),
+                                    ),
                                   ),
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => Editpage(
-                                          todo: note,
-                                          index: index,
-                                          editNote: _editNote,
+                                  Row(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.edit,
+                                          color: Color(0xff212435),
                                         ),
+                                        onPressed: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => Editpage(
+                                                todo: note,
+                                                index: index,
+                                                editNote: _editNote,
+                                              ),
+                                            ),
+                                          );
+                                        },
                                       ),
-                                    );
-                                  },
-                                ),
-                                IconButton(
-                                  icon: const Icon(
-                                    Icons.delete,
-                                    color: Color(0xff212435),
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.delete,
+                                          color: Color(0xff212435),
+                                        ),
+                                        onPressed: () {
+                                          _deleteNote(index);
+                                        },
+                                      ),
+                                    ],
                                   ),
-                                  onPressed: () {
-                                    _deleteNote(index);
-                                  },
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
